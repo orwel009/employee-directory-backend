7e983b543095dae58a88ad4de6f02fef4a2f2045
orwel009 <orwelvincent1@gmail.com>
2025-10-25 12:40:43 +0530

    schema added and backend completed

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const { ApolloServer } = require('apollo-server');
 const { connectDB } = require('./config/db');
+const typeDefs = require('./schema/typeDefs');
+const resolvers = require('./schema/resolvers');
 require('dotenv').config();
 
 async function startServer() {
   const db = await connectDB();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
 }
 
 startServer();
